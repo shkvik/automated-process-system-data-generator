9fb1ea56ade0b98c270f04a5713f6c539b59216f
pendulum <rs.pendulum@gmail.com>
2023-03-16 01:26:24 +0600

    add processes

--- lib/processes/process.py
+++ lib/processes/process.py
@@ -1,17 +1,31 @@
-
 from primitive.primitive import unit
+import datetime
 
 class algorithm:
     def __init__(self) -> None:
+        self.start: int
+        self.stop: int
+
         self.primitive = unit()
-        pass
 
 class event:
     def __init__(self) -> None:
-        self.primitive = unit()
-        pass
+        self.start: int
+        self.stop: int
+
+        self.initiators: list[algorithm]
+        self.handlers: list[algorithm]
 
 class cycle:
     def __init__(self) -> None:
-        self.primitive = unit()
+        '''
+        The cycle class includes the repetition of different algorithms, 
+        their sequence and the number of repetitions
+        '''
+
+        self.start: int
+        self.stop: int
+
+        self.repeatCounter: int
+        self.algorithms: list[algorithm]
         pass
