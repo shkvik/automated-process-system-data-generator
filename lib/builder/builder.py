020530f96eb52a80bce28cd4683c454dceb32f2f
pendulum <rs.pendulum@gmail.com>
2023-03-17 20:34:15 +0600

    good picture for watch sequence buulder

--- lib/builder/builder.py
+++ lib/builder/builder.py
@@ -1,18 +1,16 @@
 import sys
 sys.path.insert(0, 'lib')
 
+import numpy as np
+
 from processes.process import algorithm
 import matplotlib.pyplot as plt
 import random as rnd
-
-
 
 class builderAlgorithm:
     def __init__(self) -> None:
         pass
     
-
-
 class timePack:
     def __init__(self) -> None:
 
@@ -30,30 +28,43 @@ class timePack:
         return sum(self.pack.values())
 
 
-
 class builder:
     def __init__(self) -> None:
 
         self.algGrowFall    = algorithm()
         self.algBinEvent    = algorithm()
         self.algLinearEvent = algorithm()
+        self.algIndependOne    = algorithm()
+        self.algIndependTwo    = algorithm()
+
+        self.algIndependOne.primitive.linear.increase(1, 50)
+        self.algIndependTwo.primitive.linear.increase(1, 50)
 
         self.time = timePack()
         flagBinEvent: bool = False
         accumTime: int = 0
+        self.dbgTime = 172
 
-        for i in range(0, 4):
+        for i in range(0, 50):
             
-            if i % 3 == 0:
+            if i % 4 == 0 and i != 0:
                 flagBinEvent = not flagBinEvent
 
             self.variance   =  rnd.uniform(0.0, 3.0)
 
-            self.time.pack['static1']   = int(rnd.uniform(300, 500))
-            self.time.pack['grow']      = int(rnd.uniform(300, 500))
-            self.time.pack['static2']   = int(rnd.uniform(300, 500))
-            self.time.pack['fall']      = int(rnd.uniform(300, 500))
-            self.time.pack['static3']   = int(rnd.uniform(300, 500))
+            # self.time.pack['static1']   = int(rnd.uniform(300, 500))
+            # self.time.pack['grow']      = int(rnd.uniform(300, 500))
+            # self.time.pack['static2']   = int(rnd.uniform(300, 500))
+            # self.time.pack['fall']      = int(rnd.uniform(300, 500))
+            # self.time.pack['static3']   = int(rnd.uniform(300, 500))
+
+            #all day seconds = 86400
+             #sum = 2000
+            self.time.pack['static1']   = self.dbgTime
+            self.time.pack['grow']      = self.dbgTime
+            self.time.pack['static2']   = self.dbgTime
+            self.time.pack['fall']      = self.dbgTime
+            self.time.pack['static3']   = self.dbgTime
 
             self.dynGrowStaticfall(
                 self.time.pack['static1'],
@@ -62,26 +73,44 @@ class builder:
                 self.time.pack['fall'],
                 self.time.pack['static3']
             )
-
+            self.dynLinearEvent(self.time.getSum(), flagBinEvent)
             self.dynBinEvent(self.time.getSum(), flagBinEvent)
+            self.dynIndependOne(self.time.getSum())
+            self.dynIndependTwo()
+
             accumTime += self.time.getSum()
 
-    
-    def dynLinearEvent(self, time: int, activate: bool = False) -> None:
-        self.algLinearEvent.primitive.static.sequence(time)
+    def dynIndependTwo(self):
+        self.algIndependTwo.primitive.linear.increase(self.dbgTime, rnd.uniform(0, 100))
+        self.algIndependTwo.primitive.linear.increase(self.dbgTime, rnd.uniform(0, 100))
+        self.algIndependTwo.primitive.linear.increase(self.dbgTime, rnd.uniform(0, 100))
+        self.algIndependTwo.primitive.linear.increase(self.dbgTime, rnd.uniform(0, 100))
+        self.algIndependTwo.primitive.linear.increase(self.dbgTime, rnd.uniform(0, 100))
 
-        if activate:
-            self.algLinearEvent.primitive.linear.increase_pidoras(time)
+    def dynIndependOne(self, time: int):
+        self.algIndependOne.primitive.linear.increase(time, rnd.uniform(0, 100))
+
+    def dynLinearEvent(self, time: int, activate: bool = False) -> None:
+        self.algLinearEvent.primitive.setVariance(-0.1,0.1)
+
+        if activate:    
+            incr = self.algLinearEvent.primitive.parameters.getLastValue() + 30
+            incr = (incr, 100)[incr > 100]
+            self.algLinearEvent.primitive.linear.increase(time, incr)
         else:
-            self.algLinearEvent.primitive.linear.decrease(time)
+            incr = self.algLinearEvent.primitive.parameters.getLastValue() + 20
+            incr = (0, incr)[incr > 100]
+            self.algLinearEvent.primitive.linear.decrease(time, 100 - incr)
 
     def dynBinEvent(self, time: int, activate: bool = False) -> None:
-        self.algBinEvent.primitive.static.sequence(time)
+        # self.algBinEvent.primitive.static.sequence(time)
 
         if activate:
-            self.algBinEvent.primitive.static.true() 
+            self.algBinEvent.primitive.static.true()
+            self.algBinEvent.primitive.static.sequence(time)
         else:
             self.algBinEvent.primitive.static.false()
+            self.algBinEvent.primitive.static.sequence(time)
         
     def dynGrowStaticfall(self,
             static1: int,
@@ -102,15 +131,38 @@ class builder:
         return (
             self.algGrowFall.primitive.getSequence(),
             self.algBinEvent.primitive.getSequence(),
-            self.algLinearEvent.primitive.getSequence()
+            self.algLinearEvent.primitive.getSequence(),
+            self.algIndependOne.primitive.getSequence(),
+            self.algIndependTwo.primitive.getSequence()
         )
 
 
 b = builder()
 
-alg, binEvent, linEvent = b.getSequence()
+alg, binEvent, linEvent, algIndependOne, algIndependTwo = b.getSequence()
 
-plt.plot(alg)
-plt.plot(binEvent)
-plt.plot(linEvent)
+fig = plt.figure()
+
+colum = 6
+raw = 1
+
+ax_1 = fig.add_subplot(colum, raw, 1)
+ax_2 = fig.add_subplot(colum, raw, 2)
+ax_3 = fig.add_subplot(colum, raw, 3)
+ax_4 = fig.add_subplot(colum, raw, 4)
+ax_5 = fig.add_subplot(colum, raw, 5)
+ax_6 = fig.add_subplot(colum, raw, 6)
+
+ax_1.plot(alg)
+ax_2.plot(binEvent)
+ax_3.plot(linEvent)
+ax_4.plot(algIndependOne)
+ax_5.plot(algIndependTwo)
+
+ax_6.plot(alg)
+ax_6.plot(binEvent)
+ax_6.plot(linEvent)
+ax_6.plot(algIndependOne)
+ax_6.plot(algIndependTwo)
+
 plt.show()
